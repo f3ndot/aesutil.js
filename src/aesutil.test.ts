be4b1abef4df201a28ebce704e24c8dc3aa994f2
Justin Bull <me@justinbull.ca>
2023-09-21 13:18:25 -0400

    Add tests for provided IV and key

--- src/aesutil.test.ts
+++ src/aesutil.test.ts
@@ -32,6 +32,95 @@ describe("aesutil.js Tests", () => {
       expect(encryptValue("some secret")).not.toBe(encryptValue("some secret"));
     });
 
+    test("random IV generated if not provided", () => {
+      const firstEncodedIv = encryptValue("some secret").split(".")[0];
+      const secondEncodedIv = encryptValue("some secret").split(".")[0];
+      expect(firstEncodedIv).not.toBe(secondEncodedIv);
+    });
+
+    test("AESUTIL_JS_AES_ENCRYPTION_KEY used if key not provided", () => {
+      expect(() =>
+        decryptValue(
+          encryptValue("some secret"),
+          undefined,
+          process.env.AESUTIL_JS_AES_ENCRYPTION_KEY
+        )
+      ).not.toThrowError();
+    });
+
+    test("encrypts with a provided key", () => {
+      const providedKey = Buffer.from("b".repeat(32));
+
+      expect(() =>
+        decryptValue(
+          encryptValue("some secret", undefined, providedKey),
+          undefined,
+          providedKey
+        )
+      ).not.toThrowError();
+    });
+
+    test("encrypts with an encoded provided key", () => {
+      const providedKey = Buffer.from("b".repeat(32));
+      const providedEncodedKey = providedKey.toString("base64");
+
+      expect(() =>
+        decryptValue(
+          encryptValue("some secret", undefined, providedEncodedKey),
+          undefined,
+          providedKey
+        )
+      ).not.toThrowError();
+    });
+
+    test("throws if provided key string is not Base64-encoded", () => {
+      const providedKey = "!".repeat(32);
+
+      expect(() =>
+        encryptValue("some secret", undefined, providedKey)
+      ).toThrowError(Error("Key must be a Base64-encoded string"));
+    });
+
+    test("throws if provided key string is invalid length", () => {
+      const providedKey = Buffer.from("b".repeat(42));
+
+      expect(() =>
+        encryptValue("some secret", undefined, providedKey)
+      ).toThrowError(Error("Key must be 32 bytes when Base64-decoded"));
+    });
+
+    test("encrypts with an encoded IV", () => {
+      const ivBytes = Buffer.from("a".repeat(12));
+      const encodedIv = ivBytes.toString("base64");
+
+      expect(() =>
+        decryptValue(
+          encryptValue("some secret", undefined, undefined, encodedIv)
+        )
+      ).not.toThrowError();
+      expect(
+        encryptValue("some secret", undefined, undefined, encodedIv).split(
+          "."
+        )[0]
+      ).toBe(encodedIv);
+    });
+
+    test("throws if invalid IV length", () => {
+      const ivBytes = Buffer.from("a".repeat(24));
+
+      expect(() =>
+        encryptValue("some secret", undefined, undefined, ivBytes)
+      ).toThrowError(Error("Provided IV must be 12 bytes long"));
+    });
+
+    test("throws if IV string not Base64-encoded", () => {
+      const iv = "!".repeat(12);
+
+      expect(() =>
+        encryptValue("some secret", undefined, undefined, iv)
+      ).toThrowError(Error("Provided IV must be Base64-encoded if string"));
+    });
+
     test("encrypts with associated data", () => {
       const ivBytes = Buffer.from("a".repeat(12));
 
@@ -55,14 +144,73 @@ describe("aesutil.js Tests", () => {
       expect(actual).toBe("some secret");
     });
 
-    test("throws if wrong key", () => {
-      const enc_key = Buffer.from("b".repeat(32)).toString("base64");
+    test("AESUTIL_JS_AES_ENCRYPTION_KEY used if key not provided", () => {
+      expect(() =>
+        decryptValue(
+          encryptValue(
+            "some secret",
+            undefined,
+            process.env.AESUTIL_JS_AES_ENCRYPTION_KEY
+          )
+        )
+      ).not.toThrowError();
+    });
+
+    test("decrypts with a provided key", () => {
+      const providedKey = Buffer.from("c".repeat(32));
+
+      const actual = decryptValue(
+        "SwzXVdv5Gb3czS77.nYS/fv33widGKqeWC0Ci9g==.OQZQK37REbbo4n8=",
+        undefined,
+        providedKey
+      );
+      expect(actual).toBe("some secret");
+    });
+
+    test("decrypts with an encoded provided key", () => {
+      const providedKey = Buffer.from("c".repeat(32));
+      const providedEncodedKey = providedKey.toString("base64");
+
+      const actual = decryptValue(
+        "SwzXVdv5Gb3czS77.nYS/fv33widGKqeWC0Ci9g==.OQZQK37REbbo4n8=",
+        undefined,
+        providedEncodedKey
+      );
+      expect(actual).toBe("some secret");
+    });
+
+    test("throws if provided key string is not Base64-encoded", () => {
+      const providedKey = "!".repeat(32);
 
       expect(() =>
         decryptValue(
           "YWFhYWFhYWFhYWFh.8ZNQ2vxyxHKb5cGsryyFcQ==.TZKT/9YESrE8aQY=",
           undefined,
-          enc_key
+          providedKey
+        )
+      ).toThrowError(Error("Key must be a Base64-encoded string"));
+    });
+
+    test("throws if provided key is invalid length", () => {
+      const providedKey = Buffer.from("a".repeat(24));
+
+      expect(() =>
+        decryptValue(
+          "YWFhYWFhYWFhYWFh.8ZNQ2vxyxHKb5cGsryyFcQ==.TZKT/9YESrE8aQY=",
+          undefined,
+          providedKey
+        )
+      ).toThrowError(Error("Key must be 32 bytes when Base64-decoded"));
+    });
+
+    test("throws if wrong key", () => {
+      const encKey = Buffer.from("b".repeat(32));
+
+      expect(() =>
+        decryptValue(
+          "YWFhYWFhYWFhYWFh.8ZNQ2vxyxHKb5cGsryyFcQ==.TZKT/9YESrE8aQY=",
+          undefined,
+          encKey
         )
       ).toThrowError(Error("Unsupported state or unable to authenticate data"));
     });
